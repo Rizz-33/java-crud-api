01c29348805fd4882337e4bed2cb8f0937d6152b
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-06 20:16:24 +0530

    first commit

---
+++ src/main/java/rizz33/aarr/java_crud_api/JavaCrudApiApplication.java
@@ -0,0 +1,13 @@
+package rizz33.aarr.java_crud_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rizz33/aarr/java_crud_api/JavaCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rizz33.aarr.java_crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
