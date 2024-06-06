719bd05287599901f92c23585964f2463a1d6459
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-06 23:54:23 +0530

    feat: User controller

---
+++ src/main/java/rizz33/aarr/java_crud_api/controller/UserController.java
@@ -0,0 +1,20 @@
+package rizz33.aarr.java_crud_api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import rizz33.aarr.java_crud_api.model.User;
+import rizz33.aarr.java_crud_api.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+}
