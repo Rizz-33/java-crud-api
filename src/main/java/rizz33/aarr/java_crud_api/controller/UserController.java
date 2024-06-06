d2c9c7e1a66c8975ac88a10627d95a1a456e7f1c
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-06 23:57:21 +0530

    refactor: User controller

--- src/main/java/rizz33/aarr/java_crud_api/controller/UserController.java
+++ src/main/java/rizz33/aarr/java_crud_api/controller/UserController.java
@@ -1,11 +1,14 @@
 package rizz33.aarr.java_crud_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import rizz33.aarr.java_crud_api.model.User;
 import rizz33.aarr.java_crud_api.repository.UserRepository;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,5 +19,10 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
