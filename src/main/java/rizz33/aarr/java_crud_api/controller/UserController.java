04f33a355b36650444da4a3c7b1cebc9fe1acbec
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-07 11:45:40 +0530

    feat: Frontend connection in user controller

--- src/main/java/rizz33/aarr/java_crud_api/controller/UserController.java
+++ src/main/java/rizz33/aarr/java_crud_api/controller/UserController.java
@@ -1,16 +1,14 @@
 package rizz33.aarr.java_crud_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rizz33.aarr.java_crud_api.model.User;
 import rizz33.aarr.java_crud_api.repository.UserRepository;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
 
     @Autowired
